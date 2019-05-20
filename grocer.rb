797f92e44d62fd546e223c41681e866a78ad7236
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-20 13:30:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,44 @@
 def consolidate_cart(cart)
-  # code here
+  uniq_cart = cart.uniq
+  answer = {}
+  uniq_cart.each {|uni_item|
+    uni_item.each {|item_name, chars|
+      answer[item_name] = chars
+      chars[:count] = cart.count(uni_item)
+    }
+  }
+  return answer
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  if coupons.length == 0
+    return cart
+  end
+  answer = {}
+  coupons.each {|coupon|
+    cart.each {|item, chars|
+      answer[item] = chars
+      if coupon[:item] == item
+        answer[item][:count] = chars[:count] - coupon[:num]
+        answer[item + " W/COUPON"] = {price: coupon[:cost], clearance: chars[:clearance], count: 1 }
+      end
+    }
+  }
+  coupons.uniq.each {|uni_coupon|
+    if cart.has_key?(uni_coupon[:item])
+      answer[uni_coupon[:item] + " W/COUPON"][:count] = coupons.count(uni_coupon)
+    end
+  }
+  return answer
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each {|item, chars|
+    if chars[:clearance]
+      chars[:price] = (chars[:price]*0.8).round(2)
+    end
+  }
+  return cart
 end
 
 def checkout(cart, coupons)
